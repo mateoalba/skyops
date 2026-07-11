2f6b4b844f16553c5a96a48e84bd8d36f7a83d20
msteoalba <mateoalba1234gmail.com>
2026-07-11 05:35:10 -0500

    Permitir lectura de aeropuertos, aerolineas, aeronaves y puertas a cualquier usuario autenticado

--- airport/views/puerta.py
+++ airport/views/puerta.py
@@ -2,16 +2,20 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from airport.models import Puerta
 from airport.serializers import PuertaSerializer
-from airport.permissions import EsOperador
+from airport.permissions import EsOperador, SoloLectura
 from airport.filters import PuertaFilter
 
 
 class PuertaViewSet(viewsets.ModelViewSet):
     queryset = Puerta.objects.select_related("aeropuerto").all()
     serializer_class = PuertaSerializer
-    permission_classes = [EsOperador]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = PuertaFilter
     search_fields = ["codigo", "terminal"]
     ordering_fields = ["terminal", "codigo"]
     ordering = ["terminal", "codigo"]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [SoloLectura()]
+        return [EsOperador()]
